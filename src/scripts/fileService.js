c1d486ba4c8e5f9e4a14294c3eecfb12831bf6cf
HGraph Punks Patches <hgraphpunks@gmail.com>
2022-03-05 00:32:49 -0700

    update to fix display issue

--- src/scripts/fileService.js
+++ src/scripts/fileService.js
@@ -6,9 +6,24 @@ export const createSingleIPFSMetaData = async (file, metaData, nftStorageAPI) =>
   return new Promise(async (resolve, reject) => {
     const NFT_STORAGE_API_KEY = nftStorageAPI
     const nftStorageClient = new NFTStorage({ token: NFT_STORAGE_API_KEY });
+    console.log(file.type)
+    const fileType = () => {
+      switch(file.type) {
+        case 'image/png': 
+          return '.png'
+        case 'image/jpg':
+          return '.jpg'
+        case 'image/gif':
+          return '.gif'
+        case 'video/quicktime':
+            return '.mp4'
+        default:
+          return null
+      }
+    }
     const metadataCID = await nftStorageClient.store({
       ...metaData,
-      image: new File([file], `${metaData?.name}`, {
+      image: new File([file], `${metaData?.name}${fileType()}`, {
         type: file.type,
       })
     })
@@ -41,9 +56,23 @@ export const createIPFSMetaData = async (hashLipsImages, hashlipsMetaData, nftSt
     // // Crate Array for CIDs
     let metaDataCIDs = [];
       for (let index = 0; index < hashlipsMetaData.length; index++) {
+        const fileType = () => {
+          switch(sortedHashLipsImagesArray[index].type) {
+            case 'image/png': 
+              return '.png'
+            case 'image/jpg':
+              return '.jpg'
+            case 'image/gif':
+              return '.gif'
+            case 'video/quicktime':
+              return '.mp4'
+            default:
+              return null
+          }
+        }
         const metadata = await nftStorageClient.store({
           ...hashlipsMetaData[index],
-          image: new File([sortedHashLipsImagesArray[index]], `${index}`, {
+          image: new File([sortedHashLipsImagesArray[index]], `${index}${fileType()}`, {
             type: sortedHashLipsImagesArray[index].type,
           })
         })
@@ -58,7 +87,7 @@ export const uploadFile = async (event, token, setToken) => {
   /* Only support gif /png /jpg for viewing */
   const image = event?.target?.files[0]
   const base64Image= await convertToBase64(image);
-  const imgType = () => {
+  const fileType = () => {
     switch(image.type) {
       case 'image/png': 
         return '.png'
@@ -73,7 +102,7 @@ export const uploadFile = async (event, token, setToken) => {
   setToken({
       ...token,
       imageData: base64Image,
-      imageType: imgType(),
+      imageType: fileType(),
       photoSize: image.size,
   })
 }
@@ -90,6 +119,8 @@ export const uploadHashLipsFiles = async (token, setToken) => {
         return '.jpg'
       case 'image/gif':
         return '.gif'
+      case 'video/quicktime':
+        return '.mp4'
       default:
         return null
     }
